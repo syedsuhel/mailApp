f3212bd9def0f2ab0771d44c747675eda941f65e
Syed Suhel M <syedsuhel2624@gmail.com>
2023-02-21 18:43:10 +0530

    Initial code data

---
+++ Screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import { View, Button } from "react-native";
+
+function HomeScreen({ navigation }) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button
+          onPress={() => navigation.navigate('Notifications')}
+          title="Go to notifications"
+        />
+      </View>
+    );
+  }
+
+  export default HomeScreen;
---
+++ Screens/NotificationScreen.js
@@ -0,0 +1,12 @@
+import { View, Button } from "react-native";
+
+
+function NotificationsScreen({ navigation }) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button onPress={() => navigation.goBack()} title="Go back home" />
+      </View>
+    );
+  }
+
+  export default NotificationsScreen;
